5dc4c38b8f9e29f55b2f579b51b962617c34907e
Richard Barkestam <richard@barkestam.se>
2025-01-01 14:16:03 +0100

    Fixed solarcharger driver

--- drivers/solar/driver.js
+++ drivers/solar/driver.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const BaseDriver = require('../baseDriver.js');
-const { SolarCharger } = require('../../lib/devices/solar.js');
+const { Solar } = require('../../lib/devices/solar.js');
 
 class SolarChargerDriver extends BaseDriver {
 
     async onPair(session) {
-        return await super.pair(SolarCharger.totalYield, 'Solar Charger', session, true);
+        return await super.pair(Solar.totalYield, 'Solar Charger', session, true);
     }
 }
 module.exports = SolarChargerDriver;
